73922c3ca763ed324a0e891067d2164a309ae17c
Janice Taylor <notjanicetaylor@gmail.com>
2023-09-18 21:25:52 -0500

    day 33, project 7, loading json

---
+++ day33-35/Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Janice on 9/18/23.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ day33-35/Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Janice on 9/18/23.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
