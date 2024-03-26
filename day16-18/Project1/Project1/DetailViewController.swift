c84f3cc8caf086fb1fbc5c79bc6d7928c2bc5cde
Janice Taylor <notjanicetaylor@gmail.com>
2024-03-25 20:42:23 -0500

    day 49, challenge 1 -- modifying to keep track of how many times image is viewed

--- day16-18/Project1/Project1/DetailViewController.swift
+++ day16-18/Project1/Project1/DetailViewController.swift
@@ -12,14 +12,22 @@ class DetailViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     var selectedImage: String?
     var selectedPicture: Int = 0
-    var totalPictures: Int = 0 
+    var totalPictures: Int = 0
+    @IBOutlet var viewCountLabel: UILabel!
+    var viewCount: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+   
         
         title = "Picture \(selectedPicture) of \(totalPictures)"
         navigationItem.largeTitleDisplayMode = .never
-
+        
+        let defaults = UserDefaults.standard
+        let savedViewCount = defaults.integer(forKey: "viewCount")
+        
+        viewCount = savedViewCount + 1
+        save()
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
@@ -27,11 +35,21 @@ class DetailViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.hidesBarsOnTap = true
+        
+        let defaults = UserDefaults.standard
+        let savedViewCount = defaults.integer(forKey: "viewCount")
+        viewCountLabel.text = "Times viewed: \(savedViewCount)"
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
     }
+    
+    func save() {
+        let defaults = UserDefaults.standard
+        defaults.set(viewCount, forKey: "viewCount")
+    }
+    
     
 }
