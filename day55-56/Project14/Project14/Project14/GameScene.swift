12f7bc284755db56faa55a5b8539f31b885fc749
Janice Taylor <notjanicetaylor@gmail.com>
2024-03-27 16:26:34 -0500

    day 55, setting up project

---
+++ day55-56/Project14/Project14/Project14/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Janice on 3/27/24.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+     
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+    }
+  
+}
